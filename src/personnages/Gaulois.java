a238945a252956deb0c988028c767147fbd29221
Maksym Matviiv <mtm5117b@ensfsi.onmicrosoft.com>
2025-09-24 15:18:53 +0200

    Test

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,24 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	private void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+		;
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+}
